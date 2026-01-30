93df3b2d28fbc0179b33ebe0a49d7ea8e4314443
Khemchhun <khemchhun911@gmail.com>
2026-01-30 11:38:33 +0700

    update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,14 +3,11 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-// import basicSsl from '@vitejs/plugin-basic-ssl'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
-    // basicSsl(), // Disabled due to Node.js v22 compatibility issue
   ],
   resolve: {
     alias: {
